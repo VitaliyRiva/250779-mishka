6b880c4ddcc6f5961f583e2223ad23f26956d987
VitaliyRiva <annecery@yandex.ru>
2017-04-02 18:27:54 +0300

    Внес правки согласно проверке

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,22 +1,22 @@
-var navExtra = document.querySelector('.nav-features')
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navExtra = document.querySelector(".nav-features")
+var navMain = document.querySelector(".main-nav__nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav__nav--nojs");
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains("main-nav__nav--closed")) {
+    navMain.classList.remove("main-nav__nav--closed");
+    navMain.classList.add("main-nav__nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav__nav--closed");
+    navMain.classList.remove("main-nav__nav--opened");
   }
-  if (navExtra.classList.contains('main-nav--closed')) {
-      navExtra.classList.remove('main-nav--closed');
-      navExtra.classList.add('main-nav--opened');
+  if (navExtra.classList.contains("main-nav__nav--closed")) {
+      navExtra.classList.remove("main-nav__nav--closed");
+      navExtra.classList.add("main-nav__nav--opened");
   } else {
-      navExtra.classList.add('main-nav--closed');
-      navExtra.classList.remove('main-nav--opened');
+      navExtra.classList.add("main-nav__nav--closed");
+      navExtra.classList.remove("main-nav__nav--opened");
   }
 });
